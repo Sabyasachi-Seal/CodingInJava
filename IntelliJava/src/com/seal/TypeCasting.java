649cd0b04be444b84f9fd03239807160c209fc9d
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-28 13:25:59 +0530

    Type Conversion

---
+++ IntelliJava/src/com/seal/TypeCasting.java
@@ -0,0 +1,33 @@
+package com.seal;
+import java.util.Scanner;
+public class TypeCasting {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a Float: ");
+//        float num = input.nextFloat();
+//        System.out.println();
+//        System.out.print("The Number is "+num);
+//        System.out.print("Enter a Float: ");
+        //int num = input.nextInt();
+        System.out.println();
+
+        //casting the type to int
+        int num = (int)55.5f;
+        System.out.println("The Number is "+num);
+
+        //automatic type promotion in expressions
+        int a = 257;
+        byte b = (byte)a;
+        System.out.println(b);// Answer is 1 since 257 is greater than 256 , so the value is 267%256=1
+
+        byte n1 = 10;
+        byte n2 = 40;
+        byte n3 = 100;
+        int n4 = n1*n2/n3; // We can store the result of division in int since int is bigger than byte
+        System.out.println(n4);/* the multiplication result is 40*10=400 which is bigger than max byte size of 256,
+        but the intermediate result of that operation is stored int n4, so the result is 400/100=4*/
+
+        int d = 'A';
+        System.out.println(d);// It will print the ASCII value of A. It is possible only because char is smaller than int
+    }
+}
