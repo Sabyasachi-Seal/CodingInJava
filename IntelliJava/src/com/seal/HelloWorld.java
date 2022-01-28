caa2e21dc2f984f817ebee5d357f97ea0729f1b9
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-28 10:40:39 +0530

    Hello World!

--- IntelliJava/src/com/seal/HelloWorld.java
+++ IntelliJava/src/com/seal/HelloWorld.java
@@ -1,6 +1,14 @@
 package com.seal;
+
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter an Integer: ");
+//        int a = input.nextInt();
+//	    System.out.print("The number is "+a);
+        int a = 234_000_000;
+        System.out.print(a);
     }
 }
