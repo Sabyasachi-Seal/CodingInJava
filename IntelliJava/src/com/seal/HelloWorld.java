c908b1f5cb6c40201d5787f31adb3f6a8dd694af
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-28 11:07:04 +0530

    Hello World!

--- IntelliJava/src/com/seal/HelloWorld.java
+++ IntelliJava/src/com/seal/HelloWorld.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter an Integer: ");
-//        int a = input.nextInt();
-//	    System.out.print("The number is "+a);
-        int a = 234_000_000;
-        System.out.print(a);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an Integer: ");
+        int a = input.nextInt();
+	    System.out.print("The number is "+a);
+        int b = 234_000_000;
+        System.out.println(b);
+        float c = 67.67f;
+        System.out.println(c);
     }
 }
---
+++ IntelliJava/src/com/seal/Sum.java
@@ -0,0 +1,13 @@
+package com.seal;
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int sum = a+b;
+        System.out.print("The sum is "+ sum);
+
+    }
+}
