39de41c36a61a7a563c1ad18e43e0bce87d673fe
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-28 10:19:51 +0530

    Hello World!

--- IntelliJava/src/com/seal/Main.java
+++ IntelliJava/src/com/seal/Main.java
@@ -1,8 +1,6 @@
 package com.seal;
-
 public class Main {
-
     public static void main(String[] args) {
-	    System.out.println("Hello World !");
+	    System.out.println("Hello World!");
     }
 }
