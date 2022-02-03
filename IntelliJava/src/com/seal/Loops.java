db2fe14c268bade62ca2e0a38a3a53f5cb1a4555
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-02-03 21:54:47 +0530

    Loops

--- IntelliJava/src/com/seal/Loops.java
+++ IntelliJava/src/com/seal/Loops.java
@@ -7,8 +7,18 @@ public class Loops{
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the limit: ");
         int limit = input.nextInt();
-        for(int i=0; i<limit; i++){
-            System.out.println("Number: "+i);
+        for(int i=0; i<limit; i++) {
+            //System.out.println("Number: " + i);
         }
+
+        int num = limit;
+        while(num>0){
+            //System.out.print(num+" ");
+            num--;
+        }
+        do{
+            System.out.print(num+" ");
+            num++;
+        }while(num<=2*limit);
     }
 }
