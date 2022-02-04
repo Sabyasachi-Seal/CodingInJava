b162f8745e2c043efc58253a5dc143d455f8d7bc
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-02-04 18:57:50 +0530

    Methods

--- IntelliJava/src/com/seal/Methods.java
+++ IntelliJava/src/com/seal/Methods.java
@@ -1,7 +1,24 @@
 package com.seal;
-
+import java.util.Scanner;
 public class Methods {
     public static void main(String[] args){
-
+        int count = 0;
+        while(count<2){
+            greeting();
+            int ans = sum();
+            System.out.println("Sum = "+ans);
+            count++;
+        }
+    }
+    static void greeting(){
+        System.out.println("Function Call, Hello World!");
+    }
+    static int sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int ans = a+b;
+        return ans;
     }
 }
