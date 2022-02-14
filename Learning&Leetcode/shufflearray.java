a1f151e14fece155be31e02c0a1b17b26329cb54
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-02-14 09:32:26 +0530

    Shuffle Solved

--- Learning&Leetcode/shufflearray.java
+++ Learning&Leetcode/shufflearray.java
@@ -1,14 +1,31 @@
 /* https://leetcode.com/problems/shuffle-the-array/ */
 
+/* https://leetcode.com/problems/shuffle-the-array/ */
+
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2*n];
-        int j = 0;
-        for(int i=0; i<(2*n); i+=2){
-            ans[i] = nums[j];
-            ans[i+1] = nums[j+n];
-            j++;
+        for(int i=0; i<ans.length; i+=2){
+            ans[i] = nums[i/2];
+            ans[i+1] = nums[i/2 + n];
         }
     return ans;
     }
 }
+
+//
+
+// class Solution {
+//     public int[] shuffle(int[] nums, int n) {
+//         int[] ans = new int[2*n];
+//         for(int i=0; i<ans.length; i++){
+//             if(i%2==0){
+//                 ans[i] = nums[i/2];
+//             }
+//             else{
+//                 ans[i] = nums[n+i/2];
+//             }
+//         }
+//     return ans;
+//     }
+// }
