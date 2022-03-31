483a3a14979678f411bcc44407374a4f079e13fa
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-03-31 20:03:07 +0530

    leetcode

---
+++ IntelliJava/src/com/seal/contatenationarray.java
@@ -0,0 +1,13 @@
+package com.seal;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length * 2;
+        int i;
+        int arr[] = new int[n];
+        for (i=0; i<n; i++){
+            arr[i] = nums[i%nums.length];
+        }
+        return arr;
+    }
+}
