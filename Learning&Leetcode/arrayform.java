ff08b1bf93ec2803c2112aaff360f7a301989816
Sabyasachi Seal <yoboy907@gmail.com>
2022-04-19 13:51:24 +0530

    arrayform done

--- Learning&Leetcode/arrayform.java
+++ Learning&Leetcode/arrayform.java
@@ -2,29 +2,17 @@
 import java.util.*;
 import java.lang.Math;
 class arrayform {
-    public static List<Integer> addToArrayForm(int[] num, int k) {
-        long number = 0;
-        for(int dig : num){
-            // System.out.println(number);
-            number = number*10 + dig;
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> res = new LinkedList<>();
+        for (int i = A.length - 1; i >= 0; i--) {
+            res.add(0, (A[i] + K) % 10);
+            K = (A[i] + K) / 10;
         }
-
-        number += k;
-        // System.out.println(number);
-        k = (int)Math.log10(number);
-        String[] nums = new String[(int)Math.log10(number)+1];
-        List<Integer> numbers = new ArrayList<Integer>();
-
-        while(number>0){
-            nums[k] = Long.toString(number%10);
-            number /= 10;
-            k--;
+        while (K > 0) {
+            res.add(0, K % 10);
+            K /= 10;
         }
-        
-        for(String i: nums){
-            numbers.add(Integer.parseInt(i));
-        }
-        return numbers;
+        return res;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3};
