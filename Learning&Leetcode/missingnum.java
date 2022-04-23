b4eb82d89b3ea536bdeeeb8664e55216edbfe051
Sabyasachi Seal <yoboy907@gmail.com>
2022-04-23 13:52:51 +0530

    missing num

--- Learning&Leetcode/missingnum.java
+++ Learning&Leetcode/missingnum.java
@@ -2,16 +2,20 @@
 
 class missingnum {
     public static int findKthPositive(int[] arr, int k) {
-        for(int i=0; i<arr.length; i++){
-            if((arr[i]-(i+1))>=k){
-                return k+i;
+        int start = 0, end=arr.length, mid =1;
+        while(start<end){
+            mid = start + (end-start)/2;
+            if(arr[mid]-(mid+1)>=k){
+                end = mid;
+            }
+            else{
+                start = mid +1;
             }
         }
-        return arr.length+k;
-        
+        return start+k;
     }
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 7, 11};
+        int[] arr = {1, 2, 3, 4};
         int k = 5;
         System.out.println(findKthPositive(arr, k));
     }
