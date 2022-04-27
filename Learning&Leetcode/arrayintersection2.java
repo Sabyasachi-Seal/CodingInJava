968313e8bef7a9cc98d27b0103fddfd41bdfac03
Sabyasachi Seal <yoboy907@gmail.com>
2022-04-27 15:44:11 +0530

    Intersection2

--- Learning&Leetcode/arrayintersection2.java
+++ Learning&Leetcode/arrayintersection2.java
@@ -4,32 +4,42 @@ public class arrayintersection2 {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         ArrayList<Integer> ans = new ArrayList<Integer>();
-        //remove duplicates
-        for(int i=0; i<nums1.length; i++){
-            if(binarysearch(nums2, 0, nums2.length-1, nums1[i])>-1){
-                ans.add(nums2[i]);
+        for(int i=0, j=0; i<nums1.length && j<nums2.length;){
+            if(nums1[i] == nums2[j]){
+                ans.add(nums1[i]);
+                i++;
+                j++;
             }
-        }
-        int[] arr = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++)
-            arr[i] = ans.get(i);
-        return arr;
-
-    }
-    public static int binarysearch(int[]arr, int start, int end, int target){
-        int mid=0;
-        while(start<=end){
-            mid = start + (end - start)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                start = mid+1;
+            else if(nums1[i]<nums2[j]){
+                i++;
             }
             else{
-                end = mid-1;
+                j++;
             }
         }
-        return -1;
+        int smaller = min(nums1.length, nums2.length);
+        int[] arr = new int[min(smaller, ans.size())];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ans.get(i);
+        return arr;
     }
+    public static int min(int a, int b){
+        return (a<b)?a:b;
+    }
+    // public static int binarysearch(int[]arr, int start, int end, int target){
+    //     int mid=0;
+    //     while(start<=end){
+    //         mid = start + (end - start)/2;
+    //         if(arr[mid]==target){
+    //             return mid;
+    //         }
+    //         else if(arr[mid]<target){
+    //             start = mid+1;
+    //         }
+    //         else{
+    //             end = mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
