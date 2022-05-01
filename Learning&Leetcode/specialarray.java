f9f1707d460ce8227aca6370875cb6a8be853895
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-01 20:19:41 +0530

    Special Array Done

--- Learning&Leetcode/specialarray.java
+++ Learning&Leetcode/specialarray.java
@@ -2,32 +2,11 @@ import java.util.Arrays;
 // https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
 public class specialarray {
     public static int specialArray(int[] nums) {
-        int count = 0;
         Arrays.sort(nums);
-        // int ind = 0;
-        for (int i = 1; i <= nums[nums.length-1]; i++) {
-            count = 0;
-            for (int j = 0; j < nums.length;) {
-                if(i <= nums[j]){
-                    count++;
-                }
-                j++;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums.length-i <= nums[i] && (i==0 || nums.length-i>nums[i-1])){
+                return nums.length-i;
             }
-            if(count == i){
-                return count;
-            }
-            // ind = binarysearch(nums, i+1, nums.length-1, i);
-            // int temp = ind;
-            // while(ind<nums.length && nums[ind] >= i){
-            //     ind++;
-            //     count++;
-            // }
-            // if(count == temp){
-            //     return count;
-            // }
-            // else{
-            //     count = 0;
-            // }
         }
         return -1;
     }
@@ -45,7 +24,7 @@ public class specialarray {
     //             end = mid -1;
     //         }
     //     }
-    //     return end;
+    //     return start;
     // }
     public static void main(String[] args) {
         int arr[] = {3, 5};
--- Learning&Leetcode/tempCodeRunnerFile.java
+++ Learning&Leetcode/tempCodeRunnerFile.java
@@ -1,11 +1,3 @@
-  int count = 0;
-        for(int i=0; i<nums.length; i++){
-            int j = i+1;
-            while(j<nums.length && nums[j]==nums[i]){
-                count++;
-                j++;
-            }
-            nums[++i] = nums[j];
-        }
-        return nums.length - count;
-    }
+if(count == i){
+            //     return i;
+            // }
