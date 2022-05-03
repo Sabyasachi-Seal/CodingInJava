53e5a6085019f19cae89e17df186bee3eab6aea2
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-03 13:47:09 +0530

    Reach A Number

--- Learning&Leetcode/reachanumber.java
+++ Learning&Leetcode/reachanumber.java
@@ -1,13 +1,20 @@
 // https://leetcode.com/problems/reach-a-number/submissions/
 public class reachanumber {
     public static int reachNumber(int target) {
-        target = Math.abs(target);
-        int sum =0, count=0;
-        while(sum<target){
-            count++;
-            sum += count;
+        // target = Math.abs(target);
+        // int sum =0, count=0;
+        // while(sum<target){
+        //     count++;
+        //     sum += count;
+        // }
+        // return (sum-target)%2==0?count: count%2==0?count+1:count+2;
+        int step = 0, sum = 0;
+        target = Math.abs(target); 
+        while (sum < target || (sum - target) % 2 != 0) {
+            step++; 
+            sum += step;
         }
-        return (sum-target)%2==0?count: count%2==0?count+1:count+2;
+        return step; 
     }
     public static void main(String[] args) {
         System.out.println(reachNumber(2));
