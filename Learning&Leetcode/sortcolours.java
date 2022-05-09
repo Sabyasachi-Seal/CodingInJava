3fa19302428c6c417b8ce74a5d717e4953f42e2a
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-09 09:46:59 +0530

    Sort Colours O(n)

--- Learning&Leetcode/sortcolours.java
+++ Learning&Leetcode/sortcolours.java
@@ -1,18 +1,33 @@
 // https://leetcode.com/problems/sort-colors/
 public class sortcolours {
-    public void sortColors(int[] nums) {
-        boolean swapped = true;
+    public static void sortColors(int[] nums) {
+        int zeros=0, ones=0, twos=0;
+        for(int num: nums){
+            if(num==0) zeros++;
+            else if(num==1) ones++;
+            else twos++;
+        }
         for (int i = 0; i < nums.length; i++) {
-            swapped = false;
-            for (int j = 0; j < nums.length-i-1; j++) {
-                if(nums[j] > nums[j+1]){
-                    swapped = true;
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
+            if(i<zeros){
+                nums[i] = 0; 
+                continue;
             }
+            if(i<(zeros+ones)){
+                nums[i] = 1;
+                continue;
+            }
+            if(i<(zeros+ones+twos)){
+                nums[i] = 2;
+                continue;
+            }
+
         }
     }
-}
+    public static void main(String[] args) {
+        int[] arr = {2, 0 , 2, 1, 1, 0};
+        sortColors(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
