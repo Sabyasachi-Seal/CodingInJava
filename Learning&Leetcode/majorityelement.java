375b20be23c7f637cda7dc3944c18afdc14de9d3
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-11 08:24:00 +0530

    Majority Element nlogn

---
+++ Learning&Leetcode/majorityelement.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class majorityelement {
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0, i=0;
+        for(i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                count++;
+            }
+            else{
+                count++;
+                if(count > nums.length/2){
+                    break;
+                }
+                else{
+                    count = 0;
+                }
+            }
+        }
+        return nums[i];
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 3};
+        System.out.println(majorityElement(arr));
+    }
+}
