fb28b636d598f0af4f29695d09783264a64ff7c5
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-11 08:05:19 +0530

    Merge Sorted Array

---
+++ Learning&Leetcode/mergesortedarray.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/merge-sorted-array/
+public class mergesortedarray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int li=0, ri=0;
+        for (int i = 0; i < nums2.length; i++) {
+            
+        }
+    }
+}
