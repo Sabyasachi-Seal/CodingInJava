ea3eb3b20760d6a24cbeb807c9128b01fe4c0c88
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-05-11 08:30:46 +0530

    2 Lines Soln

--- Learning&Leetcode/majorityelement.java
+++ Learning&Leetcode/majorityelement.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class majorityelement {
     public static int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int count = 0, i=0;
-        for(i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]){
-                count++;
-            }
-            else{
-                count++;
-                if(count > nums.length/2){
-                    break;
-                }
-                else{
-                    count = 0;
-                }
-            }
-        }
-        return nums[i];
+        // Arrays.sort(nums);
+        // int count = 0, i=0;
+        // for(i=0; i<nums.length-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         count++;
+        //     }
+        //     else{
+        //         count++;
+        //         if(count > nums.length/2){
+        //             break;
+        //         }
+        //         else{
+        //             count = 0;
+        //         }
+        //     }
+        // }
+        // return nums[i];
+        // Arrays.sort(nums);
+	    // return nums[nums.length/2];
     }
     public static void main(String[] args) {
         int[] arr = {3, 2, 3};
