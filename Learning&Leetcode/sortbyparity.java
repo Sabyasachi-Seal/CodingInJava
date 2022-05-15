184fe20b41b7e52d51086b01d618f8ff69f23556
Sabyasachi Seal <yoboy907@gmail.com>
2022-05-15 16:14:38 +0530

    Parity Sort

--- Learning&Leetcode/sortbyparity.java
+++ Learning&Leetcode/sortbyparity.java
@@ -3,15 +3,13 @@ import java.util.*;
 public class sortbyparity {
     public int[] sortArrayByParity(int[] nums) {
         int[] ans = new int[nums.length];
-        int i, ap=0;
+        int i, start=0, end=nums.length-1;
         for (i = 0; i < ans.length; i++) {
             if((nums[i]&1)==0){
-                ans[ap++] = nums[i];
+                ans[start++] = nums[i];
             }
-        }
-        for(i=0;i<ans.length; i++){
-            if((nums[i]&1)==1){
-                ans[ap++] = nums[i];
+            else{
+                ans[end--] = nums[i];
             }
         }
         return ans;
