bc3d5ef7b873450611d0c0b48cca01c6bfbcd2e2
Sabyasachi Seal <yoboy907@gmail.com>
2022-05-16 18:46:35 +0530

    Parity 2 99%

--- Learning&Leetcode/sortbyparity2.java
+++ Learning&Leetcode/sortbyparity2.java
@@ -1,45 +1,44 @@
 // https://leetcode.com/problems/sort-array-by-parity-ii/
 public class sortbyparity2 {
-    // public static int[] sortArrayByParityII(int[] nums) {
-    //     int op = 1, ep=0;
-    //     for (int i = 0; i < nums.length; i++) {
-    //         if(nums[i]%2!=0 && i%2==0){
-    //             swap(nums, i, ep);
-    //             ep += 2;
-    //         }
-    //         if(nums[i]%2!=0 && i%2==0){
-    //             swap(nums, i, op);
-    //             op += 2;
-    //         }
-    //     }
-    //     return nums;
-    // }
-    // public static void swap(int[] arr, int a, int b){
-    //     int temp = arr[a];
-    //     arr [a] = arr[b];
-    //     arr[b] = temp;
-    // }
-    public static int[] sortArrayByParityII(int[] nums){
-        int even[] = new int[nums.length/2];
-        int ep=0;
-        int odd[] = new int[nums.length/2];
-        int op=0;
-        for (int i = 0; i < nums.length; i++) {
-            if((nums[i]&1)==0){ 
-                even[ep++] = nums[i];
+    public static int[] sortArrayByParityII(int[] nums) {
+        int op = 1, ep=0;
+        while(op<nums.length && ep<nums.length){
+            if((nums[ep]&1)==1){
+                swap(nums, op, ep);
+                op+=2;
             }
             else{
-                odd[op++] = nums[i];
+                ep += 2;
             }
-        }
-        ep=0;
-        op=0;
-        for (int i = 0; i < nums.length; i+=2) {
-            nums[i] = even[ep++];
-            nums[i+1] = odd[op++];
         }
         return nums;
     }
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr [a] = arr[b];
+        arr[b] = temp;
+    }
+    // public static int[] sortArrayByParityII(int[] nums){
+    //     int even[] = new int[nums.length/2];
+    //     int ep=0;
+    //     int odd[] = new int[nums.length/2];
+    //     int op=0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if((nums[i]&1)==0){ 
+    //             even[ep++] = nums[i];
+    //         }
+    //         else{
+    //             odd[op++] = nums[i];
+    //         }
+    //     }
+    //     ep=0;
+    //     op=0;
+    //     for (int i = 0; i < nums.length; i+=2) {
+    //         nums[i] = even[ep++];
+    //         nums[i+1] = odd[op++];
+    //     }
+    //     return nums;
+    // }
     public static void main(String[] args) {
         int[] a = {4, 2, 5, 7};
         int[] ans = sortArrayByParityII(a);
