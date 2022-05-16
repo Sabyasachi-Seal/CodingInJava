c860e38f9794931016f5205914421a3e7cca388c
Sabyasachi Seal <yoboy907@gmail.com>
2022-05-16 19:02:00 +0530

    Largest Tri 89%

--- Learning&Leetcode/largesttriangle.java
+++ Learning&Leetcode/largesttriangle.java
@@ -2,11 +2,20 @@ import java.util.Arrays;
 
 // https://leetcode.com/problems/largest-perimeter-triangle/
 public class largesttriangle {
+    // public int largestPerimeter(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i=nums.length-1; i>=2; i--){
+    //         if(nums[i]<nums[i-1]+nums[i-2]){
+    //             return nums[i]+nums[i-1]+nums[i-2];
+    //         }
+    //     }
+    //     return 0;
+    // }
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        for(int i=nums.length-1; i>=2; i--){
-            if(nums[i]<nums[i-1]+nums[i-2]){
-                return nums[i]+nums[i-1]+nums[i-2];
+        for(int i=nums.length-3; i>=0; i--){
+            if(nums[i]+nums[i+1]>nums[i+2]){
+                return nums[i]+nums[i+1]+nums[i+2];
             }
         }
         return 0;
