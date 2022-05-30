2fff04b83e814dcb9c8b7f0774a984c78343922c
Sabyasachi Seal <yoboy907@gmail.com>
2022-05-30 22:58:00 +0530

    Divide 2 Ints

--- Learning&Leetcode/longestvalidbrackets.java
+++ Learning&Leetcode/longestvalidbrackets.java
@@ -1,39 +1,6 @@
 // https://leetcode.com/problems/longest-valid-parentheses/
 import java.util.*;
 public class longestvalidbrackets {
-    // public static int longestValidParentheses(String s) {
-    //     int count = 0;
-    //     int opens = 0;
-    //     int closes = 0;
-    //     int max = 0;
-    //     for (int i = 0; i < s.length();) {
-    //         opens = 0;
-    //         closes = 0;
-    //         while(i<s.length() && s.charAt(i)=='('){
-    //             opens++;
-    //             i++;
-    //             if(i<s.length() && s.charAt(i) == ')'){
-    //                 closes++;
-    //                 count++;
-    //                 i++;
-    //             }
-    //         }
-    //         while(i<s.length() && s.charAt(i)==')'){
-    //             closes++;
-    //             i++;
-    //         }
-    //         if(opens==closes && 2*count<s.length()){
-    //             count += Math.min(opens, closes);
-    //         }
-    //         else{
-    //             count = Math.min(opens, closes);
-    //         }
-    //         if(max<count){
-    //             max = count;
-    //         }
-    //     }
-    //     return 2*max;   
-    // }
     public static int longestValidParentheses(String s){
         Stack<Character> ct = new Stack<>();
         Stack<Integer> index = new Stack<>();
