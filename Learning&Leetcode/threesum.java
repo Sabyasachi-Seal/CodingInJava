a01e9a174d8ec33ccf8f394a1b3d8fe02618de8e
Sabyasachi Seal <yoboy907@gmail.com>
2022-06-06 22:55:54 +0530

    3 Sum Accepted

--- Learning&Leetcode/threesum.java
+++ Learning&Leetcode/threesum.java
@@ -23,31 +23,57 @@ public class threesum {
     //     }
     //     return ans;
     // }
-    public static List<List<Integer>> threeSum(int[] nums) {
+    // public static List<List<Integer>> threeSum(int[] nums) {
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     Set<List<Integer>> s = new HashSet<>();
+    //     HashMap<Integer, Integer> hm = new HashMap<>();
+    //     for (int i = 0; i < nums.length; i++) {
+    //         hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+    //     }
+    //     for (int i = 0; i < nums.length; i++) {
+    //         hm.put(nums[i], hm.get(nums[i])-1);
+    //         for (int j = i+1; j < nums.length; j++) {
+    //             hm.put(nums[j], hm.get(nums[j])-1);
+    //             if(hm.containsKey(-(nums[i]+nums[j])) && hm.get(-(nums[i]+nums[j]))>0){
+    //                 List<Integer> temp =new ArrayList<Integer>();
+    //                     temp.add(nums[i]);
+    //                     temp.add(nums[j]);
+    //                     temp.add(-(nums[i]+nums[j]));
+    //                     Collections.sort(temp);
+    //                     s.add(temp);
+    //             }
+    //             hm.put(nums[j], hm.get(nums[j])+1);
+    //         }
+    //         hm.put(nums[i], hm.get(nums[i])+1);
+    //     }
+    //     for(List<Integer> lst: s){
+    //         ans.add(lst);
+    //     }
+    //     return ans;
+    // }
+    public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> ans = new ArrayList<>();
-        Set<List<Integer>> s = new HashSet<>();
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            hm.put(nums[i], hm.get(nums[i])-1);
-            for (int j = i+1; j < nums.length; j++) {
-                hm.put(nums[j], hm.get(nums[j])-1);
-                if(hm.containsKey(-(nums[i]+nums[j])) && hm.get(-(nums[i]+nums[j]))>0){
-                    List<Integer> temp =new ArrayList<Integer>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(-(nums[i]+nums[j]));
-                        Collections.sort(temp);
-                        s.add(temp);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int high = nums.length-1;
+                int low = i+1;
+                int sum = 0 - nums[i];
+                while(low<high){
+                    if(sum == nums[low]+nums[high]){
+                        ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while(low<high && (nums[low] == nums[low+1])) low++;
+                        while(low<high && (nums[high] == nums[high-1])) high--;
+                        low++; high--;
+                    }
+                    else if(sum > nums[low]+nums[high]){
+                        low++;
+                    }
+                    else{
+                        high--;
+                    }
                 }
-                hm.put(nums[j], hm.get(nums[j])+1);
             }
-            hm.put(nums[i], hm.get(nums[i])+1);
-        }
-        for(List<Integer> lst: s){
-            ans.add(lst);
         }
         return ans;
     }
