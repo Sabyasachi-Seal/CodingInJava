d7bf88a02dcdd8e7424a82ad08783dabed7616bb
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-06-27 09:45:57 +0530

    Single Num

---
+++ Learning&Leetcode/singlenum.java
@@ -0,0 +1,9 @@
+public class singlenum {
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
