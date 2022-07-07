c8626ec21b328ce5fba43fe9c9532b496eacf0f4
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-07-07 15:39:21 +0530

    Maze Problem

--- Learning&Leetcode/mazeproblem.java
+++ Learning&Leetcode/mazeproblem.java
@@ -1,7 +1,15 @@
 import java.util.*;
 public class mazeproblem {
     public static void main(String[] args) {
-        maze2("", 3, 3);
+        int row = 3, col = 3;
+        // maze2("", row, col);
+        boolean cango[][] = new boolean[row][col];
+        for (int i = 0; i < cango.length; i++) {
+            for (int j = 0; j < cango[0].length; j++) {
+                cango[i][j] = true; 
+            }
+        }
+        maze3("Start-> ", 0, 0, cango, new int[row][col], 1);
         // System.out.println(maze(new ArrayList<>(), "", 3, 3));
     }
     public static List<String> maze(List<String> ans, String path, int row, int col){
@@ -21,5 +29,34 @@ public class mazeproblem {
         if(row>1 && col>1) maze2(path+'d', row-1, col-1);
         if(row>1) maze2(path+'D', row-1, col);
         if(col>1) maze2(path+'R', row, col-1); 
+    }
+
+
+    public static void maze3(String path, int row, int col, boolean[][] cango, int arr[][], int step){
+        if(row == cango.length-1 && col == cango[0].length-1){
+            arr[row][col] = step;
+            for (int[] a : arr) {
+                System.out.println(Arrays.toString(a));
+            }
+            System.out.println(path+"Destination.");
+            System.out.println("\n");
+            return;
+        }
+
+        if(!cango[row][col]){
+            return;
+        }
+        else cango[row][col] = false;
+        
+        arr[row][col] = step;
+ 
+        if(row<cango.length-1) maze3(path+"Down-> ", row+1, col, cango, arr, step+1);
+        if(col<cango.length-1) maze3(path+"Right-> ", row, col+1, cango,arr, step+1);
+        if(row>0) maze3(path+"Up-> ", row-1, col, cango, arr, step+1);
+        if(col>0) maze3(path+"Left-> ", row, col-1, cango, arr, step+1);
+
+        cango[row][col] = true;
+        arr[row][col] = 0;
+
     }
 }
