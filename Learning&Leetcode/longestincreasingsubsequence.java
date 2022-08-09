af20cae6029e85a9a9c7a4bd621711759bb80943
Sabyasachi Seal <yoboy907@gmail.com>
2022-08-09 11:01:26 +0530

    Longest Increasing Subsequence

--- Learning&Leetcode/longestincreasingsubsequence.java
+++ Learning&Leetcode/longestincreasingsubsequence.java
@@ -1,32 +1,29 @@
-import java.util.ArrayList;
-
+import java.util.*;
 class longestincreasingsubsequence{
-    static int max = 0;
     public static void main(String[] args) {
         longestincreasingsubsequence o = new longestincreasingsubsequence();
         int []arr = {0,3,1,6,2,2,7};
         System.out.println(o.lengthOfLIS(arr));
         // System.out.println(max);
     }
+    int dp[][];
     public int lengthOfLIS(int[] nums) {
-        return subs(nums, 0, -1);
+        dp = new int[nums.length][nums.length+1];
+        for(int i=0; i<nums.length; i++){
+            Arrays.fill(dp[i], -1);    
+        }
+        return subs(nums, 0, -1)-1;
     }
-    public int subs(int arr[], int i, int prev){
+    private int subs(int arr[], int i, int prev){
         if(i == arr.length){
             return 1;
         }
-        if(prev == -1) prev = arr[i];
-        int take = 0;
-        if(arr[i] > prev) {
-            take = 1 + subs(arr, i+1, arr[i]);
+        if (dp[i][prev+1] != -1) return dp[i][prev+1];
+        int length = 0 + subs(arr, i+1, prev);
+        if(prev == -1 || arr[i] > arr[prev]) {
+            length = Math.max(length, 1 + subs(arr, i+1, i));
         }
-        int nottake = subs(arr, i+1, prev);
-        return Math.max(take, nottake);
-    }
-    public boolean isincreasing(ArrayList<Integer> ans){
-        for (int i = 0; i < ans.size()-1; i++) {
-            if(ans.get(i)>=ans.get(i+1)) return false; 
-        }
-        return true;
+        dp[i][prev+1] = length;
+        return length;
     }
 }
