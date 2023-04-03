5aab22b715946c651113fa7f0fb00ae4bfc31c8a
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2023-04-04 01:19:53 +0530

    Time: 17 ms (98.32%), Space: 50.8 MB (49.54%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -5,47 +5,28 @@ class Solution {
         
         Arrays.sort(people);
         
-        // use binary search to find out if that limit - element exists in the array 
-        
         int ans = 0;
         
-        boolean v[] = new boolean[people.length];
+        int left = 0;
+        int right = people.length-1;
         
-        for(int i=0; i<people.length; i++){
-            
-            if(v[i]) continue;
-            
-            int curr = people[i];
-            
-            // now traverse in reverse to find the best option
-            for(int j=people.length-1; j>i; j--){
-                
-                if(v[j]) continue;
-                
-                int next = people[j];
-                
-                if(next + curr <= limit){
-                    
-                    
-                    v[j] = true;
-                    v[i] = true;
-                    
-                    ans++;
-                    
-                    break;
-                }
+        // 1 2 2 3
+        
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                left++;
+                right--;   
+                ans++;
+            }
+            else{
+                right--;
+                ans++;
             }
             
         }
         
-        
-        // 1 2 4 5
-        
-        
-        for(boolean vi: v){
-            if(!vi) ans++;
-        }
         return ans;
         
     }
+    
 }
