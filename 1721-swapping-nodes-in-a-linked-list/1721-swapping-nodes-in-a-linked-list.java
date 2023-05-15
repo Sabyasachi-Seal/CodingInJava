c0843b06ee43e86424ee2b187a2238e68f0024cd
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2023-05-15 23:44:52 +0530

    Time: 2 ms (99.56%), Space: 57.9 MB (30.46%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -1,51 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        // so we have to swap the kth and the length-kth node
-
-        // if k is more than half the size of the array, then we have to loop around. so in this case we can add them up and then find the mod of them
-
-        // find length
         
-        int length = 0;
-        ListNode temp = head; 
-        while(temp != null){
-            length++;
-            temp = temp.next;
+        ListNode l = head, r = head;
+        
+        for(int i=1; i<k; i++) l = l.next;
+        
+        ListNode curr = l;
+        while(curr.next != null){
+            curr = curr.next;
+            r = r.next;    
         }
         
-        if(k > length/2){
-            k = length-k+1;
-        }
+        int t = l.val;
+        l.val = r.val;
+        r.val = t;            
         
-        int target = Math.min(length-k+1, length);
-        
-        System.out.println(length+" "+k+" "+target);
-        
-        // get the values of the 2 nodes
-        temp = head;
-        ListNode prev = null;
-        int posi = 1;
-        while(posi != k && temp!=null){
-            posi++;
-            prev = temp;
-            temp = temp.next;
-        }
-
-        ListNode t1 = (temp == null)?prev:temp;
-        int v1 = t1.val;
-
-        while(posi != target && temp!=null){
-            prev = temp;
-            temp = temp.next;
-            posi++;
-        }
-
-        temp = (temp == null)?prev:temp;
-
-        int v2 = temp.val;
-        temp.val = v1;
-        t1.val = v2;
-
         return head;
     }
 }
