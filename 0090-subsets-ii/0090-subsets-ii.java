293128584dae8fe5047eb473ad4a84e6e5a2a4af
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2023-11-25 23:40:21 +0530

    Time: 2 ms (36.24%), Space: 43.7 MB (43.41%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,27 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        return getSubsets(nums, 0, new ArrayList<>(), new ArrayList<>());
+        return getSubsets(nums, 0, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
     }
     
-    private List<List<Integer>> getSubsets(int[] arr, int ind, ArrayList<Integer> curr, List<List<Integer>> ans){
+    private List<List<Integer>> getSubsets(int[] arr, int ind, ArrayList<Integer> curr, List<List<Integer>> ans, HashSet<List<Integer>> hs){
         if(ind  == arr.length){
             
-            if(!ans.contains(curr)) ans.add(new ArrayList<>(curr));
+            if(!hs.contains(curr)){
+                
+                ArrayList<Integer> newAns = new ArrayList<>(curr);
+                
+                ans.add(newAns);
+                hs.add(newAns);
+            }
             
             return ans;
         }
         
-        getSubsets(arr, ind+1, curr, ans);
+        getSubsets(arr, ind+1, curr, ans, hs);
         
         curr.add(arr[ind]);
-        getSubsets(arr, ind+1, curr, ans);
+        getSubsets(arr, ind+1, curr, ans, hs);
         curr.remove(curr.size()-1);
         
         return ans;
